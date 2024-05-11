4ff6403ab745f5c3124e001d4865975cb0ad72d8
Soumya Sekhar Mahanti <soumyasekharmahanti@gmail.com>
2024-05-11 16:52:23 +0530

    Initial Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline font-sedan">Hello world!</h1>
+      <h1 className=" text-3xl font-dosis">THis is test</h1>
+    </>
+  );
 }
